fe074f32b8015a53856ca665fce187e4c5188de3
milirmo <110854465+milirmo@users.noreply.github.com>
2023-03-20 14:11:28 -0300

    Add files via upload

---
+++ clase 2- repetitivas/index.js
@@ -0,0 +1,40 @@
+/*
+let num=1;
+
+do {
+    document.write(num+"<br>");
+    num++
+}
+while (num<=100) {
+    document.write(num+"<br>");
+    num++;
+}
+*/
+
+
+/*
+while (num<=100) {
+    document.write(num+"<br>");
+    num++;
+}
+*/
+
+/*
+for (i=1; i<=100; i++) {
+    if (num%5===0) {
+        document.write(num+"<br>");
+    }
+    num++;
+} 
+*/
+
+let num=1
+for (a=0; a<=9; a++){
+for (i=0; i<=10; i++) {
+    document.write(num+" x "+i+"="+(num*i)+"<br>");
+} 
+i=0
+num++
+document.write("<br>");
+}
+
